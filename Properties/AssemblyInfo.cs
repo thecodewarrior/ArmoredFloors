e87568dc05cc7337333dcfcdb3ea19a7c176450d
Pierce Corcoran <pierce@plasticcow.com>
2020-03-01 11:13:34 -0800

    Dramatically simplify project structure

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ArmoredFloors")]
+[assembly: AssemblyVersion("1.0.0")]
---
+++ Source/Mod.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+using HarmonyLib;
+
+namespace ArmoredFloors
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            new Harmony("the_codewarrior.rimworld.ArmoredFloors.main").PatchAll(); 
+        }
+    }
+}
